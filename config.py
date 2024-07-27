0985da19bba334b9bf9ea8c4a70da5fe10bbce39
DARKRAMAN <aryanhyper234@gmail.com>
2024-07-27 10:42:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7321296911:AAHJZFRpLD1UlFDbD0R0hYQSMQ7dqx-5ohA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("DARKRAMAN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1259786639")
 
 
 
