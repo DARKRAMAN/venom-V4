b03861aae372e34b199406902c1a7bd1f53ff91a
DARKRAMAN <aryanhyper234@gmail.com>
2024-07-30 18:20:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7321296911:AAHJZFRpLD1UlFDbD0R0hYQSMQ7dqx-5ohA")
+BOT_TOKEN=("6960591471:AAG7E9XFBqoy1yV1ws1jouN4T3DaK0zwk6Q")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("DARKRAMAN")
